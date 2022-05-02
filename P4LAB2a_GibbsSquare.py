bb1f227a44884ce77a2488b2c0581aadf443902a
gibbse5945 <100601754+gibbse5945@users.noreply.github.com>
2022-05-02 15:43:43 -0400

    Add files via upload

---
+++ P4LAB2a_GibbsSquare.py
@@ -0,0 +1,14 @@
+import turtle           
+wn =  turtle.Screen()
+alex = turtle.Turtle()
+
+alex.forward(80)
+alex.right(70)
+alex.forward(80)
+alex.left(70)
+alex.backward(180)
+alex.left(110)
+alex.forward(80)
+alex.right(110)
+alex.forward(100)
+wn.mainloop()
---
+++ P4LAB2a_GibbsTriangle.py
@@ -0,0 +1,13 @@
+import turtle           
+wn =  turtle.Screen()
+alex = turtle.Turtle()
+
+alex.forward(140)
+alex.right(140)
+alex.forward(140)
+alex.right(80)
+alex.forward(140)
+alex.right(140)
+alex.forward(100)
+
+wn.mainloop()
